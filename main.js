affb2b718a4621855eb3c20eb78c7359ce0692fd
Mohammad Hammad <hamdev73@gmail.com>
2025-06-14 11:20:33 +0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+    var links = document.getElementById("nav-links");
+    function showMenu(){
+      links.style.right = "0px";
+    }
+    function hideMenu(){
+      links.style.right ="-200px";
+    }
